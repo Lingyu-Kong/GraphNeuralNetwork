de308baf2857b227729ef90e5bce16a7764ac4ac
Lingyu-Kong <kly20@mails.tsinghua.edu.cn>
2024-03-03 18:36:52 +0800

    update gat paper link

--- graphnn/models/gat/gat.py
+++ graphnn/models/gat/gat.py
@@ -37,6 +37,7 @@ class GANMainBlock(nn.Module):
 class GraphAttnNet(nn.Module):
     """
     Graph Attention Network
+    paper: https://arxiv.org/pdf/1710.10903.pdf
     """
     def __init__(
         self,
